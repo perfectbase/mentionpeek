73f4cdb473d383a06e686b4164f3c74b2dc45cb4
Ravi <103035973+perfectbase@users.noreply.github.com>
2025-03-16 11:22:10 +0900

    format code

--- src/routes/users.route.tsx
+++ src/routes/users.route.tsx
@@ -1,29 +1,29 @@
-import { Link, Outlet, createFileRoute } from '@tanstack/react-router'
-import axios from 'redaxios'
-import { DEPLOY_URL } from '../utils/users'
-import type { User } from '../utils/users'
+import { Link, Outlet, createFileRoute } from "@tanstack/react-router";
+import axios from "redaxios";
+import { DEPLOY_URL } from "../utils/users";
+import type { User } from "../utils/users";
 
-export const Route = createFileRoute('/users')({
+export const Route = createFileRoute("/users")({
   loader: async () => {
     return await axios
-      .get<Array<User>>(DEPLOY_URL + '/api/users')
+      .get<Array<User>>(DEPLOY_URL + "/api/users")
       .then((r) => r.data)
       .catch(() => {
-        throw new Error('Failed to fetch users')
-      })
+        throw new Error("Failed to fetch users");
+      });
   },
   component: UsersLayoutComponent,
-})
+});
 
 function UsersLayoutComponent() {
-  const users = Route.useLoaderData()
+  const users = Route.useLoaderData();
 
   return (
     <div className="p-2 flex gap-2">
       <ul className="list-disc pl-4">
         {[
           ...users,
-          { id: 'i-do-not-exist', name: 'Non-existent User', email: '' },
+          { id: "i-do-not-exist", name: "Non-existent User", email: "" },
         ].map((user) => {
           return (
             <li key={user.id} className="whitespace-nowrap">
@@ -33,16 +33,16 @@ function UsersLayoutComponent() {
                   userId: String(user.id),
                 }}
                 className="block py-1 text-blue-800 hover:text-blue-600"
-                activeProps={{ className: 'text-black font-bold' }}
+                activeProps={{ className: "text-black font-bold" }}
               >
                 <div>{user.name}</div>
               </Link>
             </li>
-          )
+          );
         })}
       </ul>
       <hr />
       <Outlet />
     </div>
-  )
+  );
 }
